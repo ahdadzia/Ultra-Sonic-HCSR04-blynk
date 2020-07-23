1866ec8ae4ef6731a7d7b0e8d743c2be1cd4c0cf
ahdadzia <68522801+ahdadzia@users.noreply.github.com>
2020-07-23 17:43:35 +0700

    Add files via upload

---
+++ sensor_jarak.ino
@@ -0,0 +1,48 @@
+#include <HCSR04.h>
+
+#define BLYNK_PRINT Serial
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+
+#define TRIGGERPIN D2
+#define ECHOPIN    D1
+
+char auth[] = "D2Vj0NMk14SuvlToJHfDVn4nYIptnt3_";
+char ssid[] = "MBAHMUKIPER";
+char pass[] = "ahhayas20";
+
+WidgetLCD lcd(V1);
+
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(TRIGGERPIN, OUTPUT);
+  pinMode(ECHOPIN, INPUT);
+  Blynk.begin(auth, ssid, pass);
+
+  lcd.clear(); 
+  lcd.print(0, 0, "Distance in cm"); 
+}
+
+void loop()
+{
+  lcd.clear();
+  lcd.print(0, 0, "Distance in cm"); 
+  long duration, distance;
+  digitalWrite(TRIGGERPIN, LOW);  
+  delayMicroseconds(3); 
+  
+  digitalWrite(TRIGGERPIN, HIGH);
+  delayMicroseconds(12); 
+  
+  digitalWrite(TRIGGERPIN, LOW);
+  duration = pulseIn(ECHOPIN, HIGH);
+  distance = (duration/2) / 29.1;
+  Serial.print(distance);
+  Serial.println("Cm");
+  lcd.print(7, 1, distance);
+  Blynk.run();
+
+  delay(3500);
+
+}
